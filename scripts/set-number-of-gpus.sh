bbb97fbf8d5fd095903fef9991077e65def58d4d
Eyal Zilberberg <eyal@run.ai>
2020-07-09 14:04:09 +0300

    fix set-number-of-gpus.sh

--- scripts/set-number-of-gpus.sh
+++ scripts/set-number-of-gpus.sh
@@ -10,14 +10,14 @@ gpus=$1
 if [[ -n "$gpus" ]]; then
     set -x
     # Change the path here 1    
-    helm upgrade -i gensynth helm-chart --set jobIndex=${GENSYNTH_JOB_INDEX},numberOfGpus=$gpus
+    helm upgrade -i gensynth helm-chart --set jobIndex=${GENSYNTH_JOB_INDEX},numberOfGpus=$gpus -f scripts/values.yaml
     kubectl delete replicaset -l app=gensynth-api
 
     set +x
     # wait for the pod to be allocated
     while [[ $(kubectl get pods -l app=gensynth-api -o go-template --template '{{range .items}}{{if eq (.status.phase) ("Pending")}}{{.metadata.name}} {{end}}{{if eq (.status.phase) ("Failed")}}{{.metadata.name}} {{end}}{{if eq (.status.phase) ("Unknown")}}{{.metadata.name}} {{end}}{{end}}') ]]; do
       if [[ "$TIMEOUT_COUNTER" -eq 30 ]]; then
-        log_error "Aborting, pod took too long to start running."
+        echo "Aborting, pod took too long to start running."
         exit 1
       fi
       echo "Not all pods are running"
